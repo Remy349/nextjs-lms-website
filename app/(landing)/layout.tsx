63befcc09ff82809e7290adbe694ffb431490b13
Remy349 <santoraga15@gmail.com>
2024-12-15 19:55:46 -0600

    feat: Add basic header component

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,8 +1,13 @@
 import "@/app/globals.css";
 import type { Metadata } from "next";
+import Header from "./_components/header/header";
+import { font } from "@/lib/fonts";
 
 export const metadata: Metadata = {
-  title: "LMS Website",
+  title: {
+    default: "LMS Website",
+    template: "%s | LMS Website",
+  },
 };
 
 export default function LandingLayout({
@@ -12,7 +17,10 @@ export default function LandingLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className={`${font.className} antialiased bg-muted/50`}>
+        <Header />
+        <main className="mt-16">{children}</main>
+      </body>
     </html>
   );
 }
