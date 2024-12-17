f2db15470be83e99104debe952802b5618fd7f36
Remy349 <santoraga15@gmail.com>
2024-12-16 18:47:34 -0600

    feat: Layout for dashboard ui

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,49 @@
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import { font } from "@/lib/fonts";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import DashboardSidebar from "./_components/sidebar/dashboard-sidebar";
+
+export const metadata: Metadata = {
+  title: "Dashboard | LMS Website",
+};
+
+export default function DashboardLayout({
+  admin,
+  student,
+  teacher,
+}: Readonly<{
+  admin: React.ReactNode;
+  student: React.ReactNode;
+  teacher: React.ReactNode;
+}>) {
+  const role: "ADMIN" | "STUDENT" | "TEACHER" = "ADMIN";
+
+  const renderNode = {
+    ADMIN: admin,
+    STUDENT: student,
+    TEACHER: teacher,
+  };
+
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased`}>
+        <SidebarProvider>
+          <DashboardSidebar />
+          <SidebarInset>
+            <header className="sticky top-0 bg-background z-50 border-b">
+              <div className="h-12 flex items-center px-6">
+                <SidebarTrigger />
+              </div>
+            </header>
+            <main className="px-6">{renderNode[role]}</main>
+          </SidebarInset>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
